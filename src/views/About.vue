3f49cb032edafcaed50a971764933e7f4cfc6e38
xavier pascual <xavierpascual@MacBooks-MacBook-Pro.local>
2021-06-25 16:07:05 +0200

    first

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,68 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Formulario</h1>
+    <form @submit.prevent="enviar">
+      <label for="nombre">Nombre</label>
+      <input 
+        type="text" 
+        placeholder="nombre"
+        v-model.trim="tarea.nombre"
+      >
+      <br>
+      <label for="tecnologías">React.js</label>
+      <input 
+        type="checkbox"
+        id="react"
+        v-model="tarea.tecnologia"
+        value="react"
+      >
+      <label for="tecnologías">Vue.js</label>
+      <input 
+        type="checkbox"
+        id="vue"
+        v-model="tarea.tecnologia"
+        value="vue"
+      >
+      <button type="submit" :disabled="bloquear">Enviar</button>
+    </form>
+    <hr>
+    <h2>Resultado</h2>
+    <p>{{tarea.nombre}}</p>
+    <p>{{tarea.tecnologia}}</p>
+    <div>
+      {{respuesta.nombre}}
+      {{respuesta.tecnologia}}
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'About',
+    data() {
+      return {
+        tarea: {
+          nombre: '',
+          tecnologia: []
+        },
+        respuesta: {}
+      }
+    },
+    methods: {
+      enviar() {
+        this.respuesta = this.tarea
+        this.tarea = {
+          nombre: '',
+          tecnologia: []
+        }
+      }
+    },
+    computed: {
+      bloquear() {
+        return this.tarea.nombre.trim() === "" ? true : false
+      }
+    }
+  }
+
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br>
+    <br>
+    <p>Form de Element ui</p>
+    <!-- <form @submit.prevent="enviar">
+        <h2>Input</h2>
+        <el-input 
+          placeholder="Please input" 
+          v-model="tarea.nombre"
+          class="input"
+          size="small"
+        ></el-input>
+        <br>
+        <h2>Checkbox</h2>
+        <el-checkbox 
+          v-model="tarea.opcion"
+          label="Opción A"
+        >
+        </el-checkbox>
+        <el-checkbox 
+          v-model.trim="tarea.opcion"
+          label="Opción B"
+        >
+        </el-checkbox>
+        <br>
+        <h2>Radio</h2>
+        <el-radio v-model="tarea.radio" label="1">Option A</el-radio>
+        <el-radio v-model="tarea.radio" label="2">Option B</el-radio>
+        <br>
+        <h2>number</h2>
+        <el-input-number 
+          v-model="tarea.num" 
+          controls-position="right" 
+          @change="handleChange" 
+        ></el-input-number>
+        <hr>
+        <el-button type="primary">Enviar</el-button>
+    </form>
+    <p>{{tarea.nombre}}</p>
+    <p>{{tarea.opcion}}</p>
+    <p>{{tarea.radio}}</p> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      tarea: {
+        nombre: '',
+        opcion: [],
+        radio: '',
+        num: 1
+      }
+    }
+  },
+  methods: {
+      handleChange(value) {
+    },
+    enviar() {
+      console.log(this.tarea)
+    }
   }
 }
 </script>
+
+<style scoped>
+  .input{
+    max-width: 300px;
+  }
+</style>
